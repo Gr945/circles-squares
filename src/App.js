bf3c90c1d6b0da9d830366bd1495ee375644a383
Grant Chobanyan <chobanyangrant@gmail.com>
2023-04-20 08:41:32 +0300

    feat

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-
-  const [circle, setCircle] = useState(false)
-  const [square, setSquare] = useState(false)
-
-  const [red, setRed] = useState(false)
-  const [blue, setBlue] = useState(false)
   const [columnNumber, setColumnNumber] = useState(1)
 
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const [filterableState, setFilterableState] = useState(state || [])
+  const [filterCollapce, setFilterCollapce] = useState(
+    {
+      circle: false, square: false, red: false, blue: false,
+      green: false, yellow: false, all: false, light: false, dark: false
+    }
+  )
 
-  useEffect (() => {
-    if(circle && square) setFilterableState(state)
-    if(circle && !square) setFilterableState(state.filter((el) => el.circle))
-    if(!circle && square) setFilterableState(state.filter((el) => !el.circle))
-    if(!circle && !square) setFilterableState([])
-    if(red) setFilterableState(filterableState.filter((el) => el.color === 'red'))
-    if(blue) setFilterableState(filterableState.filter((el) => el.color === 'blue'))
+  const [filterableState, setFilterableState] = useState([])
 
-  },[circle, square, red])
-
-
+  useEffect(() => {
+    let x = state;
+    if (!filterCollapce.red) { x = x.filter((el) => el.color !== 'red' && el.color !== 'rosybrown') }
+    if (!filterCollapce.blue) { x = x.filter((el) => el.color !== 'blue' && el.color !== 'blueviolet') }
+    if (!filterCollapce.green) { x = x.filter((el) => el.color !== 'green' && el.color !== 'greenyellow') }
+    if (!filterCollapce.yellow) { x = x.filter((el) => el.color !== 'yellow' && el.color !== 'yellowgreen') }
+    if (!filterCollapce.square) { x = x.filter((el) => el.circle) }
+    if (!filterCollapce.circle) { x = x.filter((el) => !el.circle) }
+    if (!filterCollapce.all && !filterCollapce.dark && !filterCollapce.light) { x = [] }
+    if (!filterCollapce.dark && filterCollapce.light && !filterCollapce.all) { x = x.filter((el) => !el.dark) }
+    if (filterCollapce.dark && !filterCollapce.light && !filterCollapce.all) { x = x.filter((el) => el.dark) }
+    setFilterableState(x)
+  }, [filterCollapce])
 
   return (
     <div className="App">
@@ -36,14 +39,14 @@ function App() {
       <div className='filter-circle-square'>
         <div className='filter-block'>
           <input id='circles' className='filter-figures' type='checkbox' onChange={() => {
-            setCircle(!circle)
-          }} checked={circle} />
+            setFilterCollapce({ ...filterCollapce, circle: !filterCollapce.circle })
+          }} checked={filterCollapce.circle} />
           <label for='circles' style={{ fontSize: '24px' }}>Круги</label>
         </div>
         <div className='filter-block'>
           <input id='squares' className='filter-figures' type='checkbox' onChange={() => {
-            setSquare(!square)
-          }} checked={square} />
+            setFilterCollapce({ ...filterCollapce, square: !filterCollapce.square })
+          }} checked={filterCollapce.square} />
           <label for='squares' style={{ fontSize: '24px' }}>Квадраты</label>
         </div>
       </div>
@@ -55,18 +58,60 @@ function App() {
           background: `${el.color}`
         }}></div>) : <span className='not-length'>выбранных елементов нет</span>}
         <div className='left-panel'>
-        <div className='colomn-number-block'>
-            
-            <input  className='colomn-number' type='checkbox' onChange={(e) => {
-              setRed(e.target.value)
-            }} defaultValue={red} />
+
+          <div className='colomn-number-block'>
+            <input className='colomn-number' type='checkbox' onChange={() => {
+              setFilterCollapce({ ...filterCollapce, red: !filterCollapce.red })
+            }} defaultValue={filterCollapce.red} style={{background: 'red', color: 'black'}}/>
             <label>красные</label>
           </div>
-          
+
+          <div className='colomn-number-block'>
+            <input className='colomn-number' type='checkbox' onChange={() => {
+              setFilterCollapce({ ...filterCollapce, green: !filterCollapce.green })
+            }} defaultValue={filterCollapce.green} />
+            <label>зелёный</label>
+          </div>
+
+          <div className='colomn-number-block'>
+            <input className='colomn-number' type='checkbox' onChange={() => {
+              setFilterCollapce({ ...filterCollapce, blue: !filterCollapce.blue })
+            }} defaultValue={filterCollapce.blue} />
+            <label>синий</label>
+          </div>
+
+          <div className='colomn-number-block'>
+            <input className='colomn-number' type='checkbox' onChange={() => {
+              setFilterCollapce({ ...filterCollapce, yellow: !filterCollapce.yellow })
+            }} defaultValue={filterCollapce.yellow} />
+            <label>жёлтый</label>
+          </div>
+
+          <div>
+            <div className='colomn-number-block'>
+              <input className='colomn-number' type='checkbox' onChange={() => {
+                setFilterCollapce({ ...filterCollapce, all: !filterCollapce.all })
+              }} defaultValue={filterCollapce.all} />
+              <label>Все</label>
+            </div>
+            <div className='colomn-number-block'>
+              <input className='colomn-number' type='checkbox' onChange={() => {
+                setFilterCollapce({ ...filterCollapce, dark: !filterCollapce.dark })
+              }} defaultValue={filterCollapce.dark} />
+              <label>Тёмные</label>
+            </div>
+            <div className='colomn-number-block'>
+              <input className='colomn-number' type='checkbox' onChange={() => {
+                setFilterCollapce({ ...filterCollapce, light: !filterCollapce.light })
+              }} defaultValue={filterCollapce.light} />
+              <label>Светлые</label>
+            </div>
+
+          </div>
 
           <div className='colomn-number-block'>
             <label>колонок</label>
-            <input className='colomn-number' type='number' onChange={(e) => {
+            <input className='colomn-number input-decoration' type='number' onChange={(e) => {
               setColumnNumber(e.target.value)
             }} defaultValue={columnNumber} />
           </div>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,17 +1,34 @@
-const allFigures = [{ circle: true, color: 'green' }, { circle: true, color: 'red' }, { circle: false, color: 'black' },
-{ circle: true, color: 'yellow' }, { circle: false, color: 'green' }, { circle: false, color: 'blue' }]
+const initialState = [
+    { circle: true, color: 'green', dark: true },
+    { circle: true, color: 'red', dark: true },
+    { circle: false, color: 'red', dark: true },
+    { circle: true, color: 'yellow', dark: true },
+    { circle: false, color: 'green', dark: true },
+    { circle: true, color: 'blue', dark: true },
+    { circle: false, color: 'blue', dark: true },
+    { circle: false, color: 'yellow', dark: true },
+
+    { circle: true, color: 'greenyellow', dark: false },
+    { circle: true, color: 'rosybrown', dark: false },
+    { circle: false, color: 'rosybrown', dark: false },
+    { circle: true, color: 'yellowgreen', dark: false },
+    { circle: false, color: 'greenyellow', dark: false },
+    { circle: true, color: 'blueviolet', dark: false },
+    { circle: false, color: 'blueviolet', dark: false },
+    { circle: false, color: 'yellowgreen', dark: false },
+]
 
 
-const reducer = (state = allFigures, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type) {
-        
-        case 'CIRCLE': {
-            return state.filter((el)=> el.circle)
-        }
 
-        case 'SQUERE': {
-            return state.filter((el)=> !el.circle)
-        }
+        // case 'CIRCLE': {
+        //     return state.filter((el) => el.circle)
+        // }
+
+        // case 'SQUERE': {
+        //     return state.filter((el) => !el.circle)
+        // }
 
         default:
             return state;
